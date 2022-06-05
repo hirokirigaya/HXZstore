0390bc9e6db5e74360b0161ece0c8bafcd89c976
Hiro Kirigaya <privz286@gmail.com>
2022-06-05 18:36:14 -0300

    :tada: Started Product Page

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,7 +1,30 @@
 import { Container } from "./styles";
+import {  api } from "../../services/api";
+import { useEffect, useState } from "react";
 
 export function Product() {
+
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    api.get("/products")
+    .then(response => setProducts(response.data))
+  },[])
+
   return (
-    <Container>product</Container>
+    <Container>
+    {products != undefined &&
+      <section>
+        <div className="container-product">
+        <div>
+          <img src={products[0]?.img} alt="" />
+        </div>
+        <div>
+        
+        </div>
+        </div>
+      </section>
+      }
+    </Container>
   )
 }
