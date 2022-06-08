1d320286660be0795227cd8ba95688a3f7688dc5
Hiro Kirigaya <privz286@gmail.com>
2022-06-07 21:16:10 -0300

    :tada: Test Checkbox

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
 
 const CustomCheckbox = styled.div`
-
+  display: flex;
+  align-items: center;
+  margin: .5rem;
+  gap: .5rem;
+/* 
   label {
     user-select: none;
     color: ${({ theme }) => theme.secondary};
@@ -29,6 +33,38 @@ const CustomCheckbox = styled.div`
     background-position: center;
     background-repeat: no-repeat;
     border: none;
+  } */
+  label {
+    user-select: none;
+    color: ${({ theme }) => theme.secondary};
+    cursor: pointer;
+  }
+
+  input[type = "checkbox"] {
+    appearance: none;
+    -webkit-appearance: none;
+    height: 20px;
+    width: 20px;
+    background-color: transparent;
+    border: 2px solid ${({theme}) => theme.secondary};
+    border-radius: 4px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: filter ease-in-out 0.2s;
+  }
+
+  input[type = "checkbox"]:after {
+    font-family: 'Font Awesome 5 Free';
+    content: '\f00c';
+    font-weight: 900;
+    color: ${({theme}) => theme.primary};
+    display: none;
+  }
+
+  input[type = "checkbox"]:checked:after {
+    display: block;
   }
 `
 export function Checkbox(props) {
